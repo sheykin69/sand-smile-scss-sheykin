e47400b62ba0c0e150d9d52fff3efb934b3deaa4
Daniil <sheyk.in@yandex.ru>
2021-08-18 16:35:14 +0300

    Добавлен блок Photo с каруселью, добавлено переключение между членами команды в блоке Team

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,27 @@
-console.log('script work!');
+$(document).ready(function () {
+  // слайдер для фото
+  $('.slider-photo').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+
+    prevArrow: $(".slider-arrow-left"),
+    nextArrow: $(".slider-arrow-right"),
+  });
+
+  //реализуем переключение между членами команды
+  $(".team-card").on("mouseenter", function (e) {
+    let teamIndex;
+
+    e.preventDefault();
+    $(".team-card").removeClass("active");
+    $(this).addClass("active");
+    teamIndex = $(this).index(".team-card");
+    console.log(teamIndex);
+
+    $(".team-title-js").removeClass("active");
+    $(".team-title-js").eq(teamIndex).addClass("active");
+  });
+});
